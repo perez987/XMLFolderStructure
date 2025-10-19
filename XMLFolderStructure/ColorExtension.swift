b46637a9953d41f041c1c5e1eba5f663fb7ec729
Emilio P Egido <eaperez@me.com>
2025-10-19 14:12:45 +0200

    Remove ColorExtension.swift file

--- XMLFolderStructure/ColorExtension.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-extension Color {
-	static let Navy = Color(red: 0/255, green: 0/255, blue: 128/255) // macOS calculator rgb(105,108,115)
-	static let Gray = Color(red: 100/255, green: 100/255, blue: 100/255) // macOS calculator rgb(105,108,115)
-	static let Orange = Color(red: 250/255, green: 112/255, blue: 16/255) // macOSX calculator rgb(240,145,16)
-	static let DarkGray = Color(red: 51/255, green: 51/255, blue: 51/255) // macOS calculator rgb(75,78,85)
-}
-
